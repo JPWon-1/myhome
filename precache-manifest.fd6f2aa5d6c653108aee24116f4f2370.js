81af93e2c9e54c69271a10643f16e0b8fb9be602
jongpil <rocknzero1@gmail.com>
2020-08-25 15:54:57 +0900

    Updates

---
+++ precache-manifest.fd6f2aa5d6c653108aee24116f4f2370.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0265592bbc2aa040df3d4e8d0f45be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c24c0d11c59d20760711",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "6421f0a6cd57019fc8f6",
+    "url": "/static/css/main.146ca3ef.chunk.css"
+  },
+  {
+    "revision": "c24c0d11c59d20760711",
+    "url": "/static/js/2.5f6f8f68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5f6f8f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6421f0a6cd57019fc8f6",
+    "url": "/static/js/main.56581c55.chunk.js"
+  },
+  {
+    "revision": "5aad51060861552ec732",
+    "url": "/static/js/runtime-main.0111bebd.js"
+  },
+  {
+    "revision": "2c8197d69551de2cf072c186022c0513",
+    "url": "/static/media/resume.2c8197d6.pdf"
+  },
+  {
+    "revision": "8a439cea47c4b69e244dd39d37c8bd6e",
+    "url": "/static/media/뉴욕1.8a439cea.jpg"
+  },
+  {
+    "revision": "04a6e9c3352c801e937ba81261f5cd51",
+    "url": "/static/media/호주3.04a6e9c3.jpg"
+  }
+]);
